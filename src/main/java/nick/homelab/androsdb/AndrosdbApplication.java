f2e7cd9d34999e304ef2cbce602fbbfeb001bb57
nickpanosrigas-gr <nickpanosrigas@gmail.com>
2026-02-24 12:36:04 +0200

    Initial commit: Lets GOO

---
+++ src/main/java/nick/homelab/androsdb/AndrosdbApplication.java
@@ -0,0 +1,13 @@
+package nick.homelab.androsdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndrosdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndrosdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nick/homelab/androsdb/AndrosdbApplicationTests.java
@@ -0,0 +1,13 @@
+package nick.homelab.androsdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndrosdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
